706a466d380cc8993a3919c95cbb6f25d72c9da4
idibek <alikcey.2001@gmail.com>
2025-01-09 22:16:56 +0400

    get user by id added

--- user-service/src/main/java/tj/alimov/userservice/repository/UserRepository.java
+++ user-service/src/main/java/tj/alimov/userservice/repository/UserRepository.java
@@ -4,5 +4,8 @@ package tj.alimov.userservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tj.alimov.userservice.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> getUserById(Long id);
 }
