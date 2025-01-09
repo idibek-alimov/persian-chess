2ff9381c63277b4a399ea78414a545c8009badbb
idibek <alikcey.2001@gmail.com>
2025-01-09 21:28:18 +0400

    first commit

---
+++ src/main/java/tj/alimov/persianchess/PersianChessApplication.java
@@ -0,0 +1,13 @@
+package tj.alimov.persianchess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersianChessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersianChessApplication.class, args);
+    }
+
+}
