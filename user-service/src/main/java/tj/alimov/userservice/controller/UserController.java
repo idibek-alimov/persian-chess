9ccf8cac1eefb731084eb6f44f339c21376e166c
idibek <alikcey.2001@gmail.com>
2025-01-09 21:47:31 +0400

    first simple controller added

---
+++ user-service/src/main/java/tj/alimov/userservice/controller/UserController.java
@@ -0,0 +1,17 @@
+package tj.alimov.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("")
+    public String getString(){
+        return "Hello from user";
+    }
+}
